313a34f908c992e6cb076c93c58347d252d86586
Shinrael <mircoelena89@gmail.com>
2024-05-28 16:03:58 +0200

    [add] migration, pivot, many to many

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            TypeTableSeeder::class,
-            TechnologyTableSeeder::class,
-            ProjectTableSeeder::class,
+            // TypeTableSeeder::class,
+            // TechnologyTableSeeder::class,
+            // ProjectTableSeeder::class,
+            ProjectTechnologyTableSeeder::class,
         ]);
     }
 }
